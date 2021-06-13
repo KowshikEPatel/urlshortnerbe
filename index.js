1a12c13ffef432ab7f5e219ffce95fd966f39e8a
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-13 08:55:15 +0530

    sending mail added to new user

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const randomstring = require('randomstring')
+const sgMail = require('@sendgrid/mail')
 
 const mongoclient = mongodb.MongoClient;
 const objectid = mongodb.ObjectID
@@ -13,6 +14,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 const dbURL = process.env.DB_URL
+const sendGridApi = process.env.SENDGRID_API_KEY
+sgMail.setApiKey(sendGridApi)
 
 
 app.get('/',async (req,res)=>{
@@ -40,8 +43,22 @@ app.post("/newuser",async (req,res)=>{
                     "randomString":""
                                 },
             })
-            
-            console.log(data['ops'][0]['_id'])//send the link of heroku/validate?_id object 
+            const msg = {
+                to: data['ops'][0]['username'], // Change to your recipient
+                from: 'kowshikerappajipatel@gmail.com', // Change to your verified sender
+                subject: 'Sending with SendGrid is Fun',
+                text: 'and easy to do anywhere, even with Node.js',
+                html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+              }
+              sgMail
+                .send(msg)
+                .then(() => {
+                  console.log('Email sent')
+                })
+                .catch((error) => {
+                  console.error(error)
+                })
+            //send the link of heroku/validate?_id object 
             res.status(200).json({data})
             client.close()
         })
