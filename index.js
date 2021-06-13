fd5089d2a0cefa33d01578737344e4a18b0b63af
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-13 18:19:41 +0530

    user validation and activation feature added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const randomstring = require('randomstring')
-const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer')
 
 const mongoclient = mongodb.MongoClient;
 const objectid = mongodb.ObjectID
@@ -14,8 +14,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 const dbURL = process.env.DB_URL
-const sendGridApi = process.env.SENDGRID_API_KEY
-sgMail.setApiKey(sendGridApi)
 
 
 app.get('/',async (req,res)=>{
@@ -24,6 +22,14 @@ app.get('/',async (req,res)=>{
             let user = await db.collection('user').findOne()
             let data = await db.collection("url").findOneAndUpdate({"createdBy":""},{$set:{"createdBy":user._id}})
     res.status(200).json(data)
+})
+
+app.get('/useractivate/:id', async (req,res)=>{
+  
+      const client = await mongoclient.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
+      let db = client.db('projecturlshort')
+      let user = await db.collection('user').findOneAndUpdate({"_id":req.params.id},{$set:{'isActive':true}})
+      res.redirect('https://friendly-feynman-57301c.netlify.app/useractivated') 
 })
 
 app.post("/newuser",async (req,res)=>{
@@ -44,24 +50,31 @@ app.post("/newuser",async (req,res)=>{
                     "randomString":""
                                 },
             })
-            console.log(data['ops'][0]['username'])
-            const msg = {
-                to: data['ops'][0]['username'], // Change to your recipient
-                from: 'kowshikerappajipatel@gmail.com', // Change to your verified sender
-                subject: 'microURL account validation',
-                text: '',
-                html: `<p>Hi ${data['ops'][0]['firstName']}, \n You had recently registered for microURL service.Click on the below button to activate your account and make your life hassle free with URLs </p>\n<button>Activate account</button>`
+            
+            let transporter = nodemailer.createTransport({
+              service:'gmail',
+              auth:{ 
+                user:process.env.EMAIL,
+                pass:process.env.PASSWORD
               }
-              sgMail
-                .send(msg)
-                .then(() => {
-                  console.log('Email sent')
-                })
-                .catch((error) => {
-                  console.error(error)
-                })
-            //send the link of heroku/validate?_id object 
-            res.status(200).json({data})
+            });
+
+            let mailOptions = {
+              to: data['ops'][0]['username'],
+              from :'mahesh',
+              subject: 'microURL account validation',
+              text: '',
+              html: `<p>Hi ${data['ops'][0]['firstName']}, \n You had recently registered for microURL service.Click on the below button to activate your account and make your life hassle free with URLs </p>\n <a href='https://kp-microurl.herokuapp.com/useractivate/${data['ops'][0]['_id']}'><button >Activate account</button>`
+            }
+            transporter.sendMail(mailOptions)
+            .then((response)=>{
+              console.log(response)
+              res.status(200).json({data,response})
+            })
+            .catch(err=>{console.log(err)})
+            
+            
+            
             client.close()
         })
     })
